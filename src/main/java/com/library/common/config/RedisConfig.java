a1168b5b0de1bc5791cf23f80c3ec8dea793c8e7
Ahmed Basuny <990129@saudiacatering.com>
2024-11-13 00:04:42 +0200

    add redis

--- src/main/java/com/library/common/config/RedisConfig.java
+++ src/main/java/com/library/common/config/RedisConfig.java
@@ -1,53 +1,23 @@
-package com.library.common.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.AnnotationCacheOperationSource;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.CacheInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-
-import java.time.Duration;
-
-@EnableCaching
-@Configuration
-public class RedisConfig extends CachingConfigurerSupport {
-
-    @Bean
-    @Override
-    public CacheInterceptor cacheInterceptor() {
-        CacheInterceptor cacheInterceptor = new CacheInterceptor();
-        cacheInterceptor.setCacheResolver(cacheResolver());
-        cacheInterceptor.setCacheManager(cacheManager());
-        cacheInterceptor.setKeyGenerator(keyGenerator());
-        cacheInterceptor.setErrorHandler(errorHandler());
-
-        // Custom logging advice
-        cacheInterceptor.setCacheOperationSources(
-                new AnnotationCacheOperationSource() {
-                    @Override
-                    protected void logCacheHit(Object key, Cache cache) {
-                        log.info("Cache hit for key: {}", key);
-                    }
-
-                    @Override
-                    protected void logCacheMiss(Object key) {
-                        log.info("Cache miss for key: {}", key);
-                    }
-                }
-        );
-        return cacheInterceptor;
-    }
-
+//package com.library.common.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializationContext;
+//
+//import java.time.Duration;
+//
+//@EnableCaching
+//@Configuration
+//public class RedisConfig {
+//
 //    @Value("${cache.redis.config.ttl:60}")
 //    private int ttl;
 //
@@ -67,4 +37,4 @@ public class RedisConfig extends CachingConfigurerSupport {
 //                .transactionAware()
 //                .build();
 //    }
-}
+//}
